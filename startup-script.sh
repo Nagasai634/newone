5958714bfd25ec28f9231cb62525954795982d00
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-12-09 11:14:50 +0530

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -12,7 +12,7 @@ docker run -d --cpus=1 --memory=2g \
   -e DELEGATE_TAGS="" \
   -e MANAGER_HOST_AND_PORT=https://app.harness.io us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.11.87301
 
-  docker run -d --cpus=0.1 --memory=100m \
+docker run -d --cpus=0.1 --memory=100m \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -e ACCOUNT_ID=ucHySz2jQKKWQweZdXyCog \
   -e MANAGER_HOST_AND_PORT=https://app.harness.io \
@@ -20,3 +20,10 @@ docker run -d --cpus=1 --memory=2g \
   -e UPGRADER_TOKEN=NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY= \
   -e CONTAINER_STOP_TIMEOUT=3600 \
   -e SCHEDULE="0 */1 * * *" harness/upgrader:latest
+
+
+sudo export CI_MOUNT_VOLUMES="path/to/local/cert;/etc/ssl/certs/ca-certificates.crt,path/to/local/cert2;/etc/ssl/certs/cacerts.pem"
+sudo export WORKING_DIR="/home/vardhankonderu/harness-runner"
+sudo chmod +x harness-docker-runner-linux-arm64
+sudo -E ./harness-docker-runner-linux-arm64 server
+ 
