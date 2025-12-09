1925ed40656f29abbf984efc880279670f874943
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-12-09 11:00:00 +0530

    Add startup script for Docker installation and services

---
+++ startup-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install docker.io -y
+
+docker run -d --cpus=1 --memory=2g \
+  -e DELEGATE_NAME=docker-delegate \
+  -e NEXT_GEN="true" \
+  -e DELEGATE_TYPE="DOCKER" \
+  -e ACCOUNT_ID=ucHySz2jQKKWQweZdXyCog \
+  -e DELEGATE_TOKEN=NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY= \
+  -e DELEGATE_TAGS="" \
+  -e MANAGER_HOST_AND_PORT=https://app.harness.io us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.11.87301
+
+  docker run -d --cpus=0.1 --memory=100m \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -e ACCOUNT_ID=ucHySz2jQKKWQweZdXyCog \
+  -e MANAGER_HOST_AND_PORT=https://app.harness.io \
+  -e UPGRADER_WORKLOAD_NAME=docker-delegate \
+  -e UPGRADER_TOKEN=NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY= \
+  -e CONTAINER_STOP_TIMEOUT=3600 \
+  -e SCHEDULE="0 */1 * * *" harness/upgrader:latest
